2d57ddc76c2769d0982b7dce8fd534e830e476a1
Vinay Kumar Thota <35653677+Vinukmr@users.noreply.github.com>
2022-07-01 12:10:46 +0530

    Update RegisterViewModel.cs

--- WishList/Models/AccountViewModels/RegisterViewModel.cs
+++ WishList/Models/AccountViewModels/RegisterViewModel.cs
@@ -5,6 +5,8 @@ namespace WishList.Models
 {
     public class RegisterViewModel
     {
+      [Required]
+      [EmailAddress]
       public string Email {get; set;}
       [Required]
       [DataType.Password]
